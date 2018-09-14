fb7002de8daba0fbb3163278f546d0e3d843b3f7
Трацевский Илья <deeo-sama@hotmail.com>
2018-09-14 21:33:04 +0300

    Done with zeros, again

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  if(base == 1) { return number + "!"}
+  let simpleMult = getSimpleMult(base);
+  let keys = getKeys(simpleMult);
+  let result = [];
+  keys.forEach(key => {
+    let zeros = 0;
+    for(let i = 1;  Math.pow(key, i) <= number; i++) {
+      zeros += Math.floor(number / Math.pow(key, i));
+    }
+    result.push(Math.floor(zeros/simpleMult[key]));
+  });
+  result.sort((a, b) => a - b);
+  return result[0];
+}
+
+function getSimpleMult(base) {
+  let res = {};
+  let temp = base;
+  while(temp > 1) {
+    for(let i = 2; ; i++) {
+      if(temp % i == 0) {
+        res[i] ? res[i] += 1: res[i] =1;
+        temp /= i;
+        break;
+      }
+    }
+  }
+  return res;
+}
+
+function getKeys(obj) {
+  let keys = Object.keys(obj);
+  keys.sort((a, b) => a - b);
+  return keys;
 }
